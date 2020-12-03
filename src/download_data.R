1f7641a0d0fd070214f7e6704b82ac013573e9ab
fatse <fatimeseelimi@gmail.com>
2020-12-02 21:09:59 -0800

    Refactor script 1 to consider guess_max argument in read_csv method. Autoformat the code.

--- src/download_data.R
+++ src/download_data.R
@@ -1,10 +1,10 @@
 "Downloads csv data from the web to a local filepath as a csv.
-Usage: download_data.R --url=<url> --path=<path> 
- 
+Usage: download_data.R --url=<url> --path=<path>
+
 Options:
 <url>               URL from where to download the data (must be in standard csv format)
 <path>          Path (including filename) of where to locally write the file
-"-> doc
+" -> doc
 
 library(docopt)
 library(readr)
@@ -12,7 +12,7 @@ library(readr)
 opt <- docopt(doc)
 
 main <- function(url, path) {
-  data = read_csv(url)
+  data <- read_csv(url, guess_max = 5000)
   write_csv(data, path)
 }
 
